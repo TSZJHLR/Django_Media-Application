2d1de499bc7db3ad8d9e7f199a9bf106803065e8
Sujal Ratna Tuladhar <127120496+TSZJHLR@users.noreply.github.com>
2025-01-22 23:43:26 +0545

    Delete media directory

--- media/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path 
-from . import views 
-
-urlpatterns = [
-     path('', views.index, name='index'), # home page
-     path('delete/<int:file_id>/', views.delete_file, name='delete_file'), # delete file
-     path('download/<int:file_id>/', views.download_file, name='download_file'), # download file
-]
